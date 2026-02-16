3c0f5e18b96c0006aa934cd4b45235f803f45324
YannMassoumou <YannPC>
2026-02-16 22:17:07 +0100

    SettingUp environement

--- Event_Management_System/Program.cs
+++ Event_Management_System/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Please choose of the following Events");
+
+Console.WriteLine("1.List All Events:");
+Console.WriteLine("2.List an individual event:");
+Console.WriteLine("3.Edit an event:");
+Console.WriteLine("4.Delete an event:");
+Console.WriteLine("5.List the attendees attending an event:");
+
+string selectAnEvent = Console.ReadLine();
+
+// convert selectAnEvent to int for comparison
+int selectedOption;
+if (int.TryParse(selectAnEvent, out selectedOption))
+{
+    while (selectedOption == 1)
+    {
+        selectAnEvent = Console.ReadLine();
+        selectAnEvent += "\n";
+        int.TryParse(selectAnEvent, out selectedOption);
+    }
+}
