2cabf53e1b3a084e8dc7151371242762ce2f3295
YannMassoumou <YannPC>
2026-02-17 21:09:40 +0100

    Completed Task1, Pushing PR

---
+++ Event_Management_System/Attendee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management_System
+{
+    internal class Attendee
+    {
+        public int ID {  get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+
+        public Attendee(int id, string name, string email) 
+        {
+            ID = id;
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Attendee:{Name}, Email:{Email}";
+        }
+        
+    }
+
+
+}
--- Event_Management_System/Event.cs
+++ Event_Management_System/Event.cs
@@ -8,5 +8,40 @@ namespace Event_Management_System
 {
     internal class Event
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location{ get; set; }
+        public DateTime Date { get; set; }
+
+        List<Attendee> attendees = new List<Attendee>();
+
+        public Event (int eventId, string eventName, string eventLocation, DateTime eventDate)
+        {
+            Id = eventId;
+            Name = eventName;
+            Location = eventLocation;
+            Date = eventDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Event:{Name}, Date: {Date.ToShortTimeString()},Location: {Location}, Attendee: {attendees.Count}";
+        }
+
+        public  void AddAttendee(Attendee attendee)
+        {
+            attendees.Add(attendee);
+        }
+
+        public void RemoveAttendee(Attendee attendee)
+        {
+            attendees.Remove(attendee);
+        }
+
+        public List<Attendee> GetAttendees()
+        {
+            return attendees;
+        }
+
     }
 }
