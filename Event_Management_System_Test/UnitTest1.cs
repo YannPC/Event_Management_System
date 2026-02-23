06e4429efbbb154cd8d32386bb4732b8003afffe
YannMassoumou <YannPC>
2026-02-23 22:53:11 +0100

    completed all function ready to push

---
+++ Event_Management_System_Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace Program
+{
+    public class Program
+    {
+        [Fact]
+        public void ListAllEvent()
+        {
+            // Arrange
+            Event event1 = new Event(1010, "Tech Conference", "New York", new DateTime(2026, 10, 11));
+
+
+        }
+    }
+}
